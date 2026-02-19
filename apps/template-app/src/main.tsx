986b60f3f61f7e20e1ec7b17287ecbf6af4e5077
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-02-19 18:14:07 +0000

    Add the instrument session provider - which will later be placed in separate ackage

--- apps/template-app/src/main.tsx
+++ apps/template-app/src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { DiamondTheme, ThemeProvider } from "@diamondlightsource/sci-react-ui";
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { InstrumentSessionProvider } from "./context/instrumentSession/InstrumentSessionProvider.tsx";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider theme={DiamondTheme}>
+createRoot(document.getElementById("root")!).render(
+  <InstrumentSessionProvider>
+    <StrictMode>
       <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    </StrictMode>
+  </InstrumentSessionProvider>,
+);
