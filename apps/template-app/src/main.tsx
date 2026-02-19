bcf69c6a3a157a64b5ba2bc9e53048fb80ad0495
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-02-19 17:46:30 +0000

    Add the diamond theme

--- apps/template-app/src/main.tsx
+++ apps/template-app/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { DiamondTheme, ThemeProvider } from "@diamondlightsource/sci-react-ui";
+
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={DiamondTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
