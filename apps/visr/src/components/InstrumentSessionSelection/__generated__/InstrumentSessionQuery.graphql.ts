c1c5354e58fdf9e76dcaf45f2575f94012219b8f
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-04-02 11:55:48 +0000

    re-create drh/dcs-268... branch from visr-ui, add instrument sessions query back

--- apps/visr/src/components/InstrumentSessionSelection/__generated__/InstrumentSessionQuery.graphql.ts
+++ apps/visr/src/components/InstrumentSessionSelection/__generated__/InstrumentSessionQuery.graphql.ts
@@ -8,101 +8,104 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from 'relay-runtime';
+import { ConcreteRequest } from "relay-runtime";
 export type InstrumentSessionQuery$variables = {
   instrumentName: string;
 };
 export type InstrumentSessionQuery$data = {
-  readonly instrumentByName: {
-    readonly instrumentSessions: ReadonlyArray<{
-      readonly instrumentSessionReference: string | null | undefined;
-      readonly state: string | null | undefined;
-    }>;
-  } | null | undefined;
+  readonly instrumentByName:
+    | {
+        readonly instrumentSessions: ReadonlyArray<{
+          readonly instrumentSessionReference: string | null | undefined;
+          readonly state: string | null | undefined;
+        }>;
+      }
+    | null
+    | undefined;
 };
 export type InstrumentSessionQuery = {
   response: InstrumentSessionQuery$data;
   variables: InstrumentSessionQuery$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "defaultValue": null,
-    "kind": "LocalArgument",
-    "name": "instrumentName"
-  }
-],
-v1 = [
-  {
-    "alias": null,
-    "args": [
+const node: ConcreteRequest = (function () {
+  var v0 = [
       {
-        "kind": "Variable",
-        "name": "name",
-        "variableName": "instrumentName"
-      }
+        defaultValue: null,
+        kind: "LocalArgument",
+        name: "instrumentName",
+      },
     ],
-    "concreteType": "Instrument",
-    "kind": "LinkedField",
-    "name": "instrumentByName",
-    "plural": false,
-    "selections": [
+    v1 = [
       {
-        "alias": null,
-        "args": null,
-        "concreteType": "InstrumentSession",
-        "kind": "LinkedField",
-        "name": "instrumentSessions",
-        "plural": true,
-        "selections": [
+        alias: null,
+        args: [
           {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "instrumentSessionReference",
-            "storageKey": null
+            kind: "Variable",
+            name: "name",
+            variableName: "instrumentName",
           },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "state",
-            "storageKey": null
-          }
         ],
-        "storageKey": null
-      }
-    ],
-    "storageKey": null
-  }
-];
-return {
-  "fragment": {
-    "argumentDefinitions": (v0/*: any*/),
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "InstrumentSessionQuery",
-    "selections": (v1/*: any*/),
-    "type": "Query",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": (v0/*: any*/),
-    "kind": "Operation",
-    "name": "InstrumentSessionQuery",
-    "selections": (v1/*: any*/)
-  },
-  "params": {
-    "cacheID": "a04aa356857de97c58e11e861e1b53df",
-    "id": null,
-    "metadata": {},
-    "name": "InstrumentSessionQuery",
-    "operationKind": "query",
-    "text": "query InstrumentSessionQuery(\n  $instrumentName: String!\n) {\n  instrumentByName(name: $instrumentName) {\n    instrumentSessions {\n      instrumentSessionReference\n      state\n    }\n  }\n}\n"
-  }
-};
+        concreteType: "Instrument",
+        kind: "LinkedField",
+        name: "instrumentByName",
+        plural: false,
+        selections: [
+          {
+            alias: null,
+            args: null,
+            concreteType: "InstrumentSession",
+            kind: "LinkedField",
+            name: "instrumentSessions",
+            plural: true,
+            selections: [
+              {
+                alias: null,
+                args: null,
+                kind: "ScalarField",
+                name: "instrumentSessionReference",
+                storageKey: null,
+              },
+              {
+                alias: null,
+                args: null,
+                kind: "ScalarField",
+                name: "state",
+                storageKey: null,
+              },
+            ],
+            storageKey: null,
+          },
+        ],
+        storageKey: null,
+      },
+    ];
+  return {
+    fragment: {
+      argumentDefinitions: v0 /*: any*/,
+      kind: "Fragment",
+      metadata: null,
+      name: "InstrumentSessionQuery",
+      selections: v1 /*: any*/,
+      type: "Query",
+      abstractKey: null,
+    },
+    kind: "Request",
+    operation: {
+      argumentDefinitions: v0 /*: any*/,
+      kind: "Operation",
+      name: "InstrumentSessionQuery",
+      selections: v1 /*: any*/,
+    },
+    params: {
+      cacheID: "a04aa356857de97c58e11e861e1b53df",
+      id: null,
+      metadata: {},
+      name: "InstrumentSessionQuery",
+      operationKind: "query",
+      text: "query InstrumentSessionQuery(\n  $instrumentName: String!\n) {\n  instrumentByName(name: $instrumentName) {\n    instrumentSessions {\n      instrumentSessionReference\n      state\n    }\n  }\n}\n",
+    },
+  };
 })();
 
 (node as any).hash = "72ef04fa18d968be31c314c0aa6a7011";
--- apps/visr/vitest.config.ts
+++ apps/visr/vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import baseConfig from "@atlas/vitest-conf/vitest.config";
+import react from "@vitejs/plugin-react";
 
 export default mergeConfig(
   baseConfig,
@@ -9,5 +10,18 @@ export default mergeConfig(
         exclude: ["**/RelayEnvironment.ts"],
       },
     },
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            // Enable graphql in tests
+            [
+              "babel-plugin-relay",
+              { artifactDirectory: "./src/graphql/__generated__" },
+            ],
+          ],
+        },
+      }),
+    ],
   }),
 );
