d0f018ec88272520562ad77bb3067c8cadd14c42
Douglas Winter <douglas.winter@diamond.ac.uk>
2026-06-01 14:07:19 +0000

    Add package @atlas/app-shell
    Package creates a React Router router object with common navigation and
    shell components i.e. appbar, side navigation, tabbed pages.

---
+++ packages/app-shell/src/SideNav.tsx
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Tooltip,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+import type { Section, SectionGroup } from "./Router";
+import type React from "react";
+import { Fragment } from "react";
+
+interface SideNavProps {
+  navigation: SectionGroup[];
+  open: boolean;
+}
+
+export function SideNav({ navigation, open }: SideNavProps) {
+  const width = open ? 256 : 72;
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: width,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: width,
+          boxSizing: "border-box",
+        },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: "auto" }}>
+        <List
+          sx={{
+            p: 1,
+            flexDirection: "column",
+            gap: 0.5,
+            width: "100%",
+          }}
+        >
+          {navigation.map((group, groupIndex) => (
+            <Fragment key={groupIndex}>
+              {groupIndex > 0 && <Divider />}
+              {group.sections.map((route) => (
+                <Entry key={route.path} route={route} open={open} />
+              ))}
+            </Fragment>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+}
+
+interface EntryProps {
+  route: Section;
+  open: boolean;
+}
+
+function Entry(props: EntryProps) {
+  const route = props.route;
+  const icon = (
+    <ListItemIcon
+      sx={{
+        minWidth: 0,
+        justifyContent: "center",
+        mr: props.open ? 3 : 0,
+      }}
+    >
+      {route.icon}
+    </ListItemIcon>
+  );
+  return (
+    <ListItem disablePadding>
+      <ListItemButton
+        component={NavLink as React.ElementType}
+        to={route.path}
+        sx={{
+          "&.active": {
+            bgcolor: "action.selected",
+          },
+        }}
+        aria-label={route.name}
+      >
+        {props.open ? (
+          icon
+        ) : (
+          <Tooltip title={route.name} placement="right">
+            {icon}
+          </Tooltip>
+        )}
+        <ListItemText // always render but conditionally hide
+          primary={route.name}
+          sx={{
+            opacity: props.open ? 1 : 0,
+            width: props.open ? "auto" : 0,
+            overflow: "hidden",
+            transition: "0.2s",
+          }}
+        />
+      </ListItemButton>
+    </ListItem>
+  );
+}
