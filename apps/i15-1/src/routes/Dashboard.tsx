a0ffc20250152bf291f1f397d90d75a6a78a5697
Douglas Winter <douglas.winter@diamond.ac.uk>
2026-06-03 07:59:01 +0000

    Temporarily place login button in Dashboard

--- apps/i15-1/src/routes/Dashboard.tsx
+++ apps/i15-1/src/routes/Dashboard.tsx
@@ -1,8 +1,14 @@
-import { Container, Typography, Button, Stack } from "@mui/material";
+import { Container, Typography, Stack } from "@mui/material";
 
 import InstrumentSessionView from "../components/InstrumentSessionSelection/InstrumentSessionView.tsx";
+import { useUserAuth } from "../context/userAuth/useUserAuth.ts";
+import { User } from "@diamondlightsource/sci-react-ui";
 
 function Dashboard() {
+  const user = useUserAuth();
+
+  const handleLogIn = () => window.location.assign("/oauth2/sign_in");
+  const handleLogOut = () => window.location.assign("/oauth2/sign_out");
   return (
     <>
       <Container maxWidth="sm" sx={{ mt: 5, mb: 4 }}>
@@ -10,6 +16,16 @@ function Dashboard() {
           <Typography variant="h4" component="h1" textAlign={"center"}>
             Welcome to I15-1
           </Typography>
+          <User
+            onLogin={handleLogIn}
+            onLogout={handleLogOut}
+            user={
+              user.person == null || user.person == undefined
+                ? null
+                : { fedid: user.person }
+            }
+            colour="white"
+          />
           <InstrumentSessionView
             sessionsList={[
               "cm12345-1",
