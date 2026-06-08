6c08b4ed75435ddc7b79f74dff8c0f4f6fca970b
Douglas Winter <douglas.winter@diamond.ac.uk>
2026-06-08 13:30:33 +0000

    Fix width; rename components; test divider

--- packages/app-shell/src/Layout.tsx
+++ packages/app-shell/src/Layout.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import { Outlet } from "react-router-dom";
-import { SideNav } from "./SideNav";
+import { SidebarNav } from "./SidebarNav";
 import { TopBar } from "./TopBar";
 import type { RouterProps } from "./Router";
 import { usePersistentDrawerState } from "./usePersistentDrawerState";
@@ -12,11 +12,8 @@ export function Layout(props: RouterProps) {
   return (
     <Box sx={{ display: "flex" }}>
       <TopBar title={props.title} open={open} setOpen={setOpen} />
-      <SideNav navigation={props.navigation} open={open} />
-      <Box
-        component="main"
-        sx={{ display: "flex", flex: 1, flexDirection: "column" }}
-      >
+      <SidebarNav navigation={props.navigation} open={open} />
+      <Box component="main" sx={{ flexGrow: 1 }}>
         {/* The Toolbar acts as a spacer,
             same size as the toolbar used inside the TopBar's AppBar*/}
         <Toolbar />
