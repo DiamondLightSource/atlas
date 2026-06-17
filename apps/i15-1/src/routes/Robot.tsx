05a1293761a34370d91b00504c0882f1afaf0351
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-06-17 07:29:28 +0000

    Use webcam widget from cs-web-lib

--- apps/i15-1/src/routes/Robot.tsx
+++ apps/i15-1/src/routes/Robot.tsx
@@ -6,6 +6,8 @@ import { RunPlanButton } from "@atlas/blueapi-ui";
 import { ReadOnlyPv } from "@atlas/pvws-config";
 import { StatusCard } from "../components/StatusCard";
 
+import { Webcam, newRelativePosition } from "@diamondlightsource/cs-web-lib";
+
 type RobotSampleFormData = {
   puck: number;
   position: number;
@@ -15,18 +17,7 @@ function StatusSidebar() {
   const theme = useTheme();
   return (
     <Box sx={{ ml: 5 }}>
-      <Stack direction={"column"} spacing={2}>
-        <StatusCard
-          title="Currently loaded"
-          bgColor={theme.palette.info.light}
-          cardColor={theme.palette.primary.main}
-        >
-          <ReadOnlyPv label="Puck" pv="ca://BL15J-EA-LOC-01:PUCK:INDEX" />
-          <ReadOnlyPv
-            label="Sample Pin"
-            pv="ca://BL15J-EA-LOC-01:SAMPLE:INDEX"
-          />
-        </StatusCard>
+      <Stack direction={"row"} spacing={2}>
         <StatusCard
           title="Ring status"
           bgColor={theme.palette.success.light}
@@ -45,7 +36,95 @@ function StatusSidebar() {
             units="GeV"
           />
         </StatusCard>
+        <StatusCard
+          title="EH3 Safety"
+          bgColor={theme.palette.success.light}
+          cardColor={theme.palette.primary.main}
+        >
+          <ReadOnlyPv label="Hutch Enabled" pv="ca://BL15I-PS-IOC-02:M14:LOP" />
+        </StatusCard>
+        <StatusCard
+          title="Currently loaded"
+          bgColor={theme.palette.info.light}
+          cardColor={theme.palette.primary.main}
+        >
+          <ReadOnlyPv label="Puck" pv="ca://BL15J-EA-LOC-01:PUCK:INDEX" />
+          <ReadOnlyPv
+            label="Sample Pin"
+            pv="ca://BL15J-EA-LOC-01:SAMPLE:INDEX"
+          />
+        </StatusCard>
       </Stack>
+    </Box>
+  );
+}
+
+function RobotControl() {
+  const { instrumentSession } = useInstrumentSession();
+  const [formData, setFormData] = useState<RobotSampleFormData>({
+    puck: 1,
+    position: 1,
+  });
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        padding: 5,
+        borderRadius: 1,
+        border: "1px solid",
+        borderColor: theme.palette.primary.main,
+      }}
+    >
+      <Stack direction={"column"} spacing={3} alignItems={"center"}>
+        <Typography component="h1" variant="h5">
+          Robot Control
+        </Typography>
+        <Stack direction={"row"} spacing={3} alignItems={"center"}>
+          <NumberInput
+            label="Puck"
+            numberMode="natural"
+            defaultValue={formData["puck"]}
+            onCommit={(parsedValue) => {
+              setFormData({ ...formData, ["puck"]: parsedValue });
+            }}
+          />
+          <NumberInput
+            label="Position"
+            numberMode="natural"
+            defaultValue={formData["position"]}
+            onCommit={(parsedValue) => {
+              setFormData({ ...formData, ["position"]: parsedValue });
+            }}
+          />
+        </Stack>
+        <RunPlanButton
+          name="robot_load"
+          params={formData}
+          instrumentSession={instrumentSession}
+          buttonText="Load Sample"
+        />
+        <RunPlanButton
+          name="robot_unload"
+          instrumentSession={instrumentSession}
+          buttonText="Unload Sample"
+        />
+      </Stack>
+    </Box>
+  );
+}
+
+function WebCams() {
+  const theme = useTheme();
+  const relativePos = newRelativePosition("0", "0", "250", "250");
+
+  return (
+    <Box sx={{ ml: 5 }}>
+      {" "}
+      <Webcam
+        name="Robot"
+        url="http://i15-k8s-serv-01.diamond.ac.uk:8094/JWEB1.mjpg.mjpg"
+        position={relativePos}
+      />
     </Box>
   );
 }
@@ -62,56 +141,19 @@ function Robot() {
       // component={"section"}
       sx={{
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "left",
         mt: 3,
         mr: 5,
         ml: 5,
       }}
     >
-      <Box
-        sx={{
-          padding: 5,
-          borderRadius: 1,
-          border: "1px solid",
-          borderColor: theme.palette.primary.main,
-        }}
-      >
-        <Stack direction={"column"} spacing={3} alignItems={"center"}>
-          <Typography component="h1" variant="h5">
-            Sample Position
-          </Typography>
-          <Stack direction={"row"} spacing={3} alignItems={"center"}>
-            <NumberInput
-              label="Puck"
-              numberMode="natural"
-              defaultValue={formData["puck"]}
-              onCommit={(parsedValue) => {
-                setFormData({ ...formData, ["puck"]: parsedValue });
-              }}
-            />
-            <NumberInput
-              label="Position"
-              numberMode="natural"
-              defaultValue={formData["position"]}
-              onCommit={(parsedValue) => {
-                setFormData({ ...formData, ["position"]: parsedValue });
-              }}
-            />
-          </Stack>
-          <RunPlanButton
-            name="robot_load"
-            params={formData}
-            instrumentSession={instrumentSession}
-            buttonText="Load Sample"
-          />
-          <RunPlanButton
-            name="robot_unload"
-            instrumentSession={instrumentSession}
-            buttonText="Unload Sample"
-          />
+      <Stack direction={"column"} spacing={3} alignItems={"left"}>
+        <StatusSidebar />
+        <Stack direction={"row"} spacing={3} alignItems={"left"}>
+          <WebCams />
+          <RobotControl />
         </Stack>
-      </Box>
-      <StatusSidebar />
+      </Stack>
     </Box>
   );
 }
