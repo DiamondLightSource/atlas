721193ae73fd1a1245815b74e1c0448dd1e0f7f9
Abigail Yates <abigail.yates@diamond.ac.uk>
2026-06-19 10:31:09 +0000

    wip - view one static slice in z-direction

--- apps/visr/src/components/tomography/SliceViewer.tsx
+++ apps/visr/src/components/tomography/SliceViewer.tsx
@@ -1,10 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SliceRenderer from "./SliceRenderer";
+import { Box } from "@mui/material";
 
 enum Plane {
   X,
   Y,
   Z,
+}
+interface Volume {
+  volumeData: Uint8Array;
+  volumeShape: [number, number, number];
 }
 
 interface Props {
@@ -16,7 +21,28 @@ interface Props {
 
 export default function SliceViewer() {
   // add props
+  const [volume, setVolume] = useState<Volume | null>(null);
 
+  useEffect(() => {
+    async function loadTestVolume() {
+      const [metaRes, rawRes] = await Promise.all([
+        fetch("/test-data/volume.json"),
+        fetch("/test-data/volume.raw"),
+      ]);
+      const meta = (await metaRes.json()) as {
+        shape: [number, number, number];
+      };
+      const buffer = await rawRes.arrayBuffer();
+      setVolume({
+        volumeData: new Uint8Array(buffer),
+        volumeShape: meta.shape,
+      });
+    }
+
+    loadTestVolume();
+  }, []);
+
+  console.log(volume?.volumeData);
   //////////////////////// dummy array /////////////////////////
   const sliceraw = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -31,9 +57,9 @@ export default function SliceViewer() {
     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 10, 10, 11, 9, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0, 0, 82, 83, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 74, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
     [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0],
     [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
@@ -42,10 +68,24 @@ export default function SliceViewer() {
   ];
   const slice = sliceraw.map(row => row.map(value => value * 255));
   /////////////////////////////////////////////////////////////
+  const firstSlice = volume?.volumeData.slice(91 * 91 * 64, 91 * 91 * 65);
+  console.log(firstSlice);
+  if (firstSlice == undefined) {
+    return <Box />;
+  }
+  function TwoDimensional(arr: Uint8Array, size: number) {
+    var res = [];
+    for (var i = 0; i < arr.length; i = i + size)
+      res.push(arr.slice(i, i + size));
+    return res;
+  }
+  const firstSlice2D: number[][] = [].slice.call(
+    TwoDimensional(firstSlice, 91),
+  );
+  console.log(firstSlice2D);
+  // useEffect(() => {
+  //   // maths to find the correct slice from the volume
+  // }, []); // useffect should be retriggered by a change in choice of plane or depth (which will be changed form the controls)
 
-  useEffect(() => {
-    // maths to find the correct slice from the volume
-  }, []); // useffect should be retriggered by a change in choice of plane or depth (which will be changed form the controls)
-
-  return <SliceRenderer slicearray={slice} />;
+  return <SliceRenderer slicearray={firstSlice2D} />;
 }
