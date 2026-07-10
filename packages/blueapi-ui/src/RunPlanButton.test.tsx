60dfcc924736c819ee7c863baafdcf5ae76c9e81
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-07-10 13:45:42 +0000

    Add a test

--- packages/blueapi-ui/src/RunPlanButton.test.tsx
+++ packages/blueapi-ui/src/RunPlanButton.test.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { render, screen, within, userEvent } from "@atlas/vitest-conf";
+import {
+  render,
+  screen,
+  within,
+  userEvent,
+  queryByText,
+  getByText,
+} from "@atlas/vitest-conf";
 import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
 import { RunPlanButton } from "./RunPlanButton";
 import { BlueapiProvider } from "@atlas/blueapi-query";
@@ -8,7 +15,8 @@ import {
   renderWithProviders,
   useSubmitTask,
 } from "@atlas/blueapi-query";
-import type { Api } from "@atlas/blueapi";
+import type { Api, TaskResponse } from "@atlas/blueapi";
+import { Snackbar } from "@mui/material";
 
 describe("RunPlanButton", () => {
   let queryClient: QueryClient;
@@ -41,5 +49,34 @@ describe("RunPlanButton", () => {
       </BlueapiProvider>,
     );
     expect(screen.getByText("Run"));
+  });
+
+  it("presses button", () => {
+    const mockResponse: TaskResponse = {
+      task_id: "92e6a0c3-52ff-4161-84ec-73096697e571",
+    };
+    vi.mocked(api.worker.setActiveTask).mockResolvedValue(mockResponse);
+
+    render(
+      <BlueapiProvider api={api}>
+        <QueryClientProvider client={queryClient}>
+          <RunPlanButton
+            name="test_plan"
+            params={[]}
+            instrumentSession="cm12345-1"
+          />
+        </QueryClientProvider>
+        ,
+      </BlueapiProvider>,
+    );
+    // screen.getByText("Run").click();
+
+    const runPlanButton = screen.getByText("Run");
+    const user = userEvent.setup();
+    user.click(runPlanButton);
+    expect(screen.queryByText("successful")).toBeInTheDocument();
+
+    // const message = queryByText("successful");
+    // expect(message).toBeInTheDocument();
   });
 });
