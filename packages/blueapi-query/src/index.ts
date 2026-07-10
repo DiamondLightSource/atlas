2f11b3093bb12878be4e107e893f24c5740336fe
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-07-10 13:07:06 +0000

    export renderWithProviders from blueapi-query, fix tests

--- packages/blueapi-query/src/index.ts
+++ packages/blueapi-query/src/index.ts
@@ -2,5 +2,6 @@ export * from "./plans";
 export * from "./tasks";
 export * from "./worker";
 export * from "./devices";
+export * from "./testUtils";
 
 export { BlueapiProvider, useBlueapi } from "./provider";
--- packages/blueapi-ui/src/RunPlanButton.test.tsx
+++ packages/blueapi-ui/src/RunPlanButton.test.tsx
@@ -1,36 +1,45 @@
 import * as React from "react";
 import { render, screen, within, userEvent } from "@atlas/vitest-conf";
+import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
 import { RunPlanButton } from "./RunPlanButton";
-
-// This doesn't actually mock the functions, and complains about
-// `useQueryClient`, and then `useBlueapi`
-// vi.mock("@/atlas/blueapi-query/tasks/useSubmitTask", () => {
-//   return {
-//     useSubmitTask: vi.fn(),
-//   };
-// });
-
-// vi.mock("@/atlas/blueapi-query/tasks/useSetActiveTask", () => {
-//   return {
-//     useSetActiveTask: vi.fn(),
-//   };
-// });
-
-// This doesn't actually mock the functions, and complains about
-// `useQueryClient`, and then `useBlueapi`
-// const useSubmitTask = vi.fn();
-// const useSetActiveTask = vi.fn();
+import { BlueapiProvider } from "@atlas/blueapi-query";
+import {
+  createTestQueryClient,
+  renderWithProviders,
+  useSubmitTask,
+} from "@atlas/blueapi-query";
+import type { Api } from "@atlas/blueapi";
 
 describe("RunPlanButton", () => {
+  let queryClient: QueryClient;
+  let api: Api;
+
+  beforeEach(() => {
+    queryClient = createTestQueryClient();
+
+    api = {
+      tasks: {
+        submit: vi.fn(),
+      },
+      worker: {
+        setActiveTask: vi.fn(),
+      },
+    } as unknown as Api;
+  });
+
   it("renders default button with Run Plan", () => {
     render(
-      <RunPlanButton
-        name="test_plan"
-        params={[]}
-        instrumentSession="cm12345-1"
-      />,
+      <BlueapiProvider api={api}>
+        <QueryClientProvider client={queryClient}>
+          <RunPlanButton
+            name="test_plan"
+            params={[]}
+            instrumentSession="cm12345-1"
+          />
+        </QueryClientProvider>
+        ,
+      </BlueapiProvider>,
     );
-
-    expect(screen.getByText("Run Plan"));
+    expect(screen.getByText("Run"));
   });
 });
