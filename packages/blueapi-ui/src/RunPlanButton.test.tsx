62a43c21aca6b32e6cdbf4b12a8f300960c85029
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-07-14 11:14:31 +0000

    Implement hook mocks

--- packages/blueapi-ui/src/RunPlanButton.test.tsx
+++ packages/blueapi-ui/src/RunPlanButton.test.tsx
@@ -3,20 +3,16 @@ import { render, screen } from "@atlas/vitest-conf";
 import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
 import { RunPlanButton } from "./RunPlanButton";
 import { BlueapiProvider } from "@atlas/blueapi-query";
+import { useGetWorkerState, useSubmitTask } from "@atlas/blueapi-query";
 import { createTestQueryClient } from "@atlas/blueapi-query";
 import type { Api, TaskResponse } from "@atlas/blueapi";
 
-function renderComponentWithProviders(
-  apiMock: Api,
-  queryClient: QueryClient,
-  children: React.ReactNode,
-) {
-  render(
-    <BlueapiProvider api={apiMock}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </BlueapiProvider>,
-  );
-}
+vi.mock("@atlas/blueapi-query");
+const workerStateMock = vi.mocked(useGetWorkerState);
+const submitTaskMock = vi.mocked(useSubmitTask);
+const mockResponse: TaskResponse = {
+  task_id: "92e6a0c3-52ff-4161-84ec-73096697e571",
+};
 
 describe("RunPlanButton", () => {
   let queryClient: QueryClient;
@@ -35,22 +31,24 @@ describe("RunPlanButton", () => {
   });
 
   it("renders default button with Run", () => {
-    renderComponentWithProviders(
-      api,
-      queryClient,
+    workerStateMock.mockReturnValue({ data: "IDLE" } as any);
+    submitTaskMock.mockReturnValue({ data: mockResponse } as any);
+
+    render(
       <RunPlanButton
         name="test_plan"
         params={[]}
         instrumentSession="cm12345-1"
       />,
     );
+
     expect(screen.getByText("Run"));
   });
 
   it("renders button with custom text", () => {
-    renderComponentWithProviders(
-      api,
-      queryClient,
+    workerStateMock.mockReturnValue({ data: "IDLE" } as any);
+    submitTaskMock.mockReturnValue({ data: mockResponse } as any);
+    render(
       <RunPlanButton
         name="test_plan"
         params={[]}
@@ -61,39 +59,41 @@ describe("RunPlanButton", () => {
     expect(screen.getByText("My Button Text"));
   });
 
-  it("success message appears when button is pressed with successful response", () => {
-    const mockResponse: TaskResponse = {
-      task_id: "92e6a0c3-52ff-4161-84ec-73096697e571",
-    };
-    vi.mocked(api.worker.setActiveTask).mockResolvedValue(mockResponse);
-
-    renderComponentWithProviders(
-      api,
-      queryClient,
+  it("renders button as diabled when worker is busy", () => {
+    workerStateMock.mockReturnValue({ data: "RUNNING" } as any);
+    render(
       <RunPlanButton
         name="test_plan"
         params={[]}
         instrumentSession="cm12345-1"
       />,
     );
+    expect(screen.getByText("Run")).toBeDisabled();
+  });
 
+  it("success message appears when button is pressed with successful response", () => {
+    workerStateMock.mockReturnValue({ data: "IDLE" } as any);
+    submitTaskMock.mockReturnValue({ data: mockResponse } as any);
+    render(
+      <RunPlanButton
+        name="test_plan"
+        params={[]}
+        instrumentSession="cm12345-1"
+      />,
+    );
     screen.getByText("Run").click();
     expect(screen.findByText("Plan submission successful!"));
   });
 
   it("failure message appears when button is pressed with failed response", () => {
-    vi.mocked(api.worker.setActiveTask).mockRejectedValue;
-
-    renderComponentWithProviders(
-      api,
-      queryClient,
+    submitTaskMock.mockRejectedValue;
+    render(
       <RunPlanButton
         name="test_plan"
         params={[]}
         instrumentSession="cm12345-1"
       />,
     );
-
     screen.getByText("Run").click();
     expect(screen.findByTestId("Plan submission failed!"));
   });
