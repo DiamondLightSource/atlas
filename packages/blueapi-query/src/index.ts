05ef5022ac074cac21f666cd81a1557bc11c58c9
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-07-14 11:16:45 +0000

    remove added export from blueapi-query

--- packages/blueapi-query/src/index.ts
+++ packages/blueapi-query/src/index.ts
@@ -2,6 +2,5 @@ export * from "./plans";
 export * from "./tasks";
 export * from "./worker";
 export * from "./devices";
-export * from "./testUtils";
 
 export { BlueapiProvider, useBlueapi } from "./provider";
