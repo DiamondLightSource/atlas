94f04cff17232b46902a9b86b6ccfc7f298d7b3a
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-07-16 16:08:27 +0000

    Remove some extra comments

--- packages/blueapi-ui/src/RunPlanButton.tsx
+++ packages/blueapi-ui/src/RunPlanButton.tsx
@@ -50,16 +50,12 @@ export function RunPlanButton({
   const runTask = async (task_id: string) => {
     await startTask.mutateAsync(task_id).then(async (response) => {
       if (response) {
-        setSeverity("info");
-        setMsg("Running plan!");
         let status = await blueapi.worker.getState();
         while (status !== idleState && status !== abortState) {
-          console.log(`status: ${status}`);
           await waitForIdle(100);
           status = await blueapi.worker.getState();
         }
         const data = await blueapi.tasks.get(task_id);
-        console.log(data);
         if (data.is_complete) {
           if (data.outcome?.outcome === "success") {
             setSeverity("success");
