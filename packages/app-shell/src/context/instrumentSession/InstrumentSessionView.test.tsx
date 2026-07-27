abe88e468ff39641701597cf182c94c74333b2f5
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-07-27 13:40:09 +0000

    fix tests

--- packages/app-shell/src/context/instrumentSession/InstrumentSessionView.test.tsx
+++ packages/app-shell/src/context/instrumentSession/InstrumentSessionView.test.tsx
@@ -1,47 +1,54 @@
-import { render, screen } from "@atlas/vitest-conf";
+import { render, screen, userEvent } from "@atlas/vitest-conf";
 import { InstrumentSessionView } from "./InstrumentSessionView";
 import {
   InstrumentSessionProvider,
   useInstrumentSession,
 } from "./InstrumentSessionProvider";
 import { useState, type ReactNode } from "react";
-import { ID_STORAGE_KEY } from "./InstrumentSessionProvider";
 
-function renderComponentWithProvider(children: ReactNode) {
-  return <InstrumentSessionProvider>{children}</InstrumentSessionProvider>;
+function renderComponentWithProvider() {
+  return render(
+    <InstrumentSessionProvider>
+      <InstrumentSessionView />
+    </InstrumentSessionProvider>,
+  );
 }
 
 // mock use instrument session
-const mockIS = ({return localStorage.getItem(ID_STORAGE_KEY)
+// const mockUseIS = vi.fn(() => ({
+//   instrumentSession: "cm12345-1",
+//   setInstrumentSession: vi.fn(),
+//   sessionsList: ["cm123-4", "cm567-8"],
+// }));
+vi.mock(import("./InstrumentSessionProvider"), async (importOriginal) => {
+  const actual =
+    await importOriginal<typeof import("./InstrumentSessionProvider")>();
+  return {
+    ...actual,
+    useInstrumentSession: () => ({
+      instrumentSession: "cm12345-1",
+      setInstrumentSession: vi.fn(),
+      sessionsList: ["cm123-4", "cm567-8"],
+    }),
+  };
 });
-const setMockIS = (instrumentSession: string) => {
-  localStorage.setItem(ID_STORAGE_KEY, instrumentSession);
-};
-const mockUseIS = () => ({
-  instrumentSession: mockIS,
-  setInstrumentSession: setMockIS,
-  sessionsList: ["cm123-4", "cm567-8"],
-});
-vi.mock("./InstrumentSessionProvider", () => ({
-  useInstrumentSession: () => mockUseIS(),
-  ID_STORAGE_KEY: "instrument-session-id",
-}));
 
 describe("InstrumentSessionView", () => {
   beforeEach(() => {
     localStorage.clear();
   });
 
-  it("reads instrument session from localStorage", () => {
-    localStorage.setItem(ID_STORAGE_KEY, "cm12345-1");
-    const { instrumentSession, setInstrumentSession, sessionsList } =
-      useInstrumentSession();
-    expect(instrumentSession).toBe("cm12345-1");
+  it("shows current instrument session", () => {
+    renderComponentWithProvider();
+    expect(screen.getByText("cm12345-1")).toBeInTheDocument();
   });
 
-  //   it("renders the instrument session provided", () => {
-  //     localStorage.setItem(ID_STORAGE_KEY, "cm12345-1");
-  //     renderComponentWithProvider(<InstrumentSessionView />);
-  //     expect(screen.getAllByText("cm12345-1"));
-  //   });
+  it("shows sessions from the provided sessions list when clicked", async () => {
+    renderComponentWithProvider();
+
+    const user = userEvent.setup();
+    await user.click(screen.getByText("cm12345-1"));
+    expect(screen.getByText("cm123-4")).toBeInTheDocument();
+    expect(screen.getByText("cm567-8")).toBeInTheDocument();
+  });
 });
