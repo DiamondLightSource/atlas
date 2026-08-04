6a6d1f022d54b13b541c804d2d04f045bc756be8
Douglas Winter <douglas.winter@diamond.ac.uk>
2026-08-04 13:53:03 +0000

    App shell improvements

--- packages/app-shell/src/Layout.tsx
+++ packages/app-shell/src/Layout.tsx
@@ -1,19 +1,37 @@
-import Box from "@mui/material/Box";
-import { Outlet } from "react-router-dom";
-import { SideNav } from "./SideNav";
-import { TopBar } from "./TopBar";
-import type { RouterProps } from "./Router";
-import { usePersistentDrawerState } from "./usePersistentDrawerState";
 import { Toolbar } from "@mui/material";
+import Box from "@mui/material/Box";
+import { NavLink, Outlet } from "react-router-dom";
+import { routePath, type RouterProps } from "./Router";
+import { SidebarNav, type Navigation } from "./SidebarNav";
+import { TopBar } from "./TopBar";
+import { usePersistentDrawerState } from "./usePersistentDrawerState";
+
+export function toNavItemGroups(routerProps: RouterProps): Navigation {
+  return routerProps.navigation.map((group) => ({
+    name: group.name,
+    navItems: group.sections.map((section) => ({
+      label: section.name,
+      icon: section.icon,
+      linkProps: {
+        to: routePath(section),
+        component: NavLink,
+      },
+    })),
+  }));
+}
+
 export function Layout(props: RouterProps) {
   const { open, setOpen } = usePersistentDrawerState();
+
+  const navigation = toNavItemGroups(props);
 
   return (
     <Box sx={{ display: "flex" }}>
       <TopBar title={props.title} open={open} setOpen={setOpen} />
-      <SideNav
-        navigation={props.navigation}
+      <SidebarNav
+        navigation={navigation}
         open={open}
+        setOpen={setOpen}
         footer={props.footer}
       />
       <Box
