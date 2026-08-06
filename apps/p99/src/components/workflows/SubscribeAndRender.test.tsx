b8b75624f1b9b881ad5a94fa1981bbf22a868065
NatLeung96 <nathanael.leung96@gmail.com>
2026-08-06 14:49:41 +0000

    add test for SubscribeAndRender

---
+++ apps/p99/src/components/workflows/SubscribeAndRender.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { RelayEnvironmentProvider } from "react-relay";
+import { MemoryRouter } from "react-router-dom";
+import { RelayEnvironment } from "../../context/workflows/RelayEnvironment";
+import SubscribeAndRender from "./SubscribeAndRender";
+
+describe("SubscribeAndRender", () => {
+  it("renders", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]} initialIndex={0}>
+        <RelayEnvironmentProvider environment={RelayEnvironment}>
+          <SubscribeAndRender
+          result={{type: "success", message: "test"}}
+          visit={{proposalCode: "ab", proposalNumber: 12345, number: 1}}
+          workflowName="ptypy-p99-from-config"
+          index={0}
+          />
+        </RelayEnvironmentProvider>
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText("test")).toBeInTheDocument();
+    expect(screen.getByTestId("status-icon-unknown")).toBeInTheDocument();
+  })
+  
+})
