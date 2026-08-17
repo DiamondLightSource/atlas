4f411d99ce62115607632800682d3823a5de28e2
Abigail Yates <abigail.yates@diamond.ac.uk>
2026-08-17 10:24:24 +0000

    remove typography import to fix linting

--- apps/visr/src/components/tomography/SliceViewer.tsx
+++ apps/visr/src/components/tomography/SliceViewer.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { Plane } from "./PlaneEnum";
 import { HeatmapPlot } from "@diamondlightsource/davidia";
 import ndarray from "ndarray";
