dc25bce8b8047b6817a11b2bdfb6bda3b165db3e
NatLeung96 <nathanael.leung96@gmail.com>
2026-08-18 15:30:23 +0000

    fix linting

--- apps/p99/src/components/workflows/__generated__/SubmissionFormTestQuery.graphql.ts
+++ apps/p99/src/components/workflows/__generated__/SubmissionFormTestQuery.graphql.ts
@@ -8,7 +8,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from 'relay-runtime';
+import { ConcreteRequest } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type SubmissionFormTestQuery$variables = Record<PropertyKey, never>;
 export type SubmissionFormTestQuery$data = {
@@ -21,118 +21,118 @@ export type SubmissionFormTestQuery = {
   variables: SubmissionFormTestQuery$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "kind": "Literal",
-    "name": "name",
-    "value": "ptypy-p99-from-config"
-  }
-];
-return {
-  "fragment": {
-    "argumentDefinitions": [],
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "SubmissionFormTestQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v0/*: any*/),
-        "concreteType": "WorkflowTemplate",
-        "kind": "LinkedField",
-        "name": "workflowTemplate",
-        "plural": false,
-        "selections": [
-          {
-            "args": null,
-            "kind": "FragmentSpread",
-            "name": "workflowTemplateFragment"
-          }
-        ],
-        "storageKey": "workflowTemplate(name:\"ptypy-p99-from-config\")"
-      }
-    ],
-    "type": "Query",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": [],
-    "kind": "Operation",
-    "name": "SubmissionFormTestQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v0/*: any*/),
-        "concreteType": "WorkflowTemplate",
-        "kind": "LinkedField",
-        "name": "workflowTemplate",
-        "plural": false,
-        "selections": [
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "name",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "maintainer",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "title",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "description",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "arguments",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "uiSchema",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "repository",
-            "storageKey": null
-          }
-        ],
-        "storageKey": "workflowTemplate(name:\"ptypy-p99-from-config\")"
-      }
-    ]
-  },
-  "params": {
-    "cacheID": "4d401cb76dd7347f2e3f617f3b6170ae",
-    "id": null,
-    "metadata": {},
-    "name": "SubmissionFormTestQuery",
-    "operationKind": "query",
-    "text": "query SubmissionFormTestQuery {\n  workflowTemplate(name: \"ptypy-p99-from-config\") {\n    ...workflowTemplateFragment\n  }\n}\n\nfragment workflowTemplateFragment on WorkflowTemplate {\n  name\n  maintainer\n  title\n  description\n  arguments\n  uiSchema\n  repository\n}\n"
-  }
-};
+const node: ConcreteRequest = (function () {
+  var v0 = [
+    {
+      kind: "Literal",
+      name: "name",
+      value: "ptypy-p99-from-config",
+    },
+  ];
+  return {
+    fragment: {
+      argumentDefinitions: [],
+      kind: "Fragment",
+      metadata: null,
+      name: "SubmissionFormTestQuery",
+      selections: [
+        {
+          alias: null,
+          args: v0 /*: any*/,
+          concreteType: "WorkflowTemplate",
+          kind: "LinkedField",
+          name: "workflowTemplate",
+          plural: false,
+          selections: [
+            {
+              args: null,
+              kind: "FragmentSpread",
+              name: "workflowTemplateFragment",
+            },
+          ],
+          storageKey: 'workflowTemplate(name:"ptypy-p99-from-config")',
+        },
+      ],
+      type: "Query",
+      abstractKey: null,
+    },
+    kind: "Request",
+    operation: {
+      argumentDefinitions: [],
+      kind: "Operation",
+      name: "SubmissionFormTestQuery",
+      selections: [
+        {
+          alias: null,
+          args: v0 /*: any*/,
+          concreteType: "WorkflowTemplate",
+          kind: "LinkedField",
+          name: "workflowTemplate",
+          plural: false,
+          selections: [
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "name",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "maintainer",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "title",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "description",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "arguments",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "uiSchema",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "repository",
+              storageKey: null,
+            },
+          ],
+          storageKey: 'workflowTemplate(name:"ptypy-p99-from-config")',
+        },
+      ],
+    },
+    params: {
+      cacheID: "4d401cb76dd7347f2e3f617f3b6170ae",
+      id: null,
+      metadata: {},
+      name: "SubmissionFormTestQuery",
+      operationKind: "query",
+      text: 'query SubmissionFormTestQuery {\n  workflowTemplate(name: "ptypy-p99-from-config") {\n    ...workflowTemplateFragment\n  }\n}\n\nfragment workflowTemplateFragment on WorkflowTemplate {\n  name\n  maintainer\n  title\n  description\n  arguments\n  uiSchema\n  repository\n}\n',
+    },
+  };
 })();
 
 (node as any).hash = "5bc0a249228ca92dcbce2d40e75ee1dd";
--- apps/p99/src/graphql/__generated__/templateViewQuery.graphql.ts
+++ apps/p99/src/graphql/__generated__/templateViewQuery.graphql.ts
@@ -8,7 +8,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from 'relay-runtime';
+import { ConcreteRequest } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type templateViewQuery$variables = {
   templateName: string;
@@ -23,125 +23,125 @@ export type templateViewQuery = {
   variables: templateViewQuery$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "defaultValue": null,
-    "kind": "LocalArgument",
-    "name": "templateName"
-  }
-],
-v1 = [
-  {
-    "kind": "Variable",
-    "name": "name",
-    "variableName": "templateName"
-  }
-];
-return {
-  "fragment": {
-    "argumentDefinitions": (v0/*: any*/),
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "templateViewQuery",
-    "selections": [
+const node: ConcreteRequest = (function () {
+  var v0 = [
       {
-        "alias": null,
-        "args": (v1/*: any*/),
-        "concreteType": "WorkflowTemplate",
-        "kind": "LinkedField",
-        "name": "workflowTemplate",
-        "plural": false,
-        "selections": [
-          {
-            "args": null,
-            "kind": "FragmentSpread",
-            "name": "workflowTemplateFragment"
-          }
-        ],
-        "storageKey": null
-      }
+        defaultValue: null,
+        kind: "LocalArgument",
+        name: "templateName",
+      },
     ],
-    "type": "Query",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": (v0/*: any*/),
-    "kind": "Operation",
-    "name": "templateViewQuery",
-    "selections": [
+    v1 = [
       {
-        "alias": null,
-        "args": (v1/*: any*/),
-        "concreteType": "WorkflowTemplate",
-        "kind": "LinkedField",
-        "name": "workflowTemplate",
-        "plural": false,
-        "selections": [
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "name",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "maintainer",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "title",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "description",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "arguments",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "uiSchema",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "repository",
-            "storageKey": null
-          }
-        ],
-        "storageKey": null
-      }
-    ]
-  },
-  "params": {
-    "cacheID": "19fc0d9ac63aa4eab9e10aefa178227c",
-    "id": null,
-    "metadata": {},
-    "name": "templateViewQuery",
-    "operationKind": "query",
-    "text": "query templateViewQuery(\n  $templateName: String!\n) {\n  workflowTemplate(name: $templateName) {\n    ...workflowTemplateFragment\n  }\n}\n\nfragment workflowTemplateFragment on WorkflowTemplate {\n  name\n  maintainer\n  title\n  description\n  arguments\n  uiSchema\n  repository\n}\n"
-  }
-};
+        kind: "Variable",
+        name: "name",
+        variableName: "templateName",
+      },
+    ];
+  return {
+    fragment: {
+      argumentDefinitions: v0 /*: any*/,
+      kind: "Fragment",
+      metadata: null,
+      name: "templateViewQuery",
+      selections: [
+        {
+          alias: null,
+          args: v1 /*: any*/,
+          concreteType: "WorkflowTemplate",
+          kind: "LinkedField",
+          name: "workflowTemplate",
+          plural: false,
+          selections: [
+            {
+              args: null,
+              kind: "FragmentSpread",
+              name: "workflowTemplateFragment",
+            },
+          ],
+          storageKey: null,
+        },
+      ],
+      type: "Query",
+      abstractKey: null,
+    },
+    kind: "Request",
+    operation: {
+      argumentDefinitions: v0 /*: any*/,
+      kind: "Operation",
+      name: "templateViewQuery",
+      selections: [
+        {
+          alias: null,
+          args: v1 /*: any*/,
+          concreteType: "WorkflowTemplate",
+          kind: "LinkedField",
+          name: "workflowTemplate",
+          plural: false,
+          selections: [
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "name",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "maintainer",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "title",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "description",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "arguments",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "uiSchema",
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "repository",
+              storageKey: null,
+            },
+          ],
+          storageKey: null,
+        },
+      ],
+    },
+    params: {
+      cacheID: "19fc0d9ac63aa4eab9e10aefa178227c",
+      id: null,
+      metadata: {},
+      name: "templateViewQuery",
+      operationKind: "query",
+      text: "query templateViewQuery(\n  $templateName: String!\n) {\n  workflowTemplate(name: $templateName) {\n    ...workflowTemplateFragment\n  }\n}\n\nfragment workflowTemplateFragment on WorkflowTemplate {\n  name\n  maintainer\n  title\n  description\n  arguments\n  uiSchema\n  repository\n}\n",
+    },
+  };
 })();
 
 (node as any).hash = "a095c2f1a9314457db4bf9b26bc541a2";
