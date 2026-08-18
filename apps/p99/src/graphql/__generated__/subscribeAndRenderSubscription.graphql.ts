8469b9975dd3007beccc0ce9df067c04f218cb67
NatLeung96 <nathanael.leung96@gmail.com>
2026-08-18 15:30:23 +0000

    fix linting

--- apps/p99/src/graphql/__generated__/subscribeAndRenderSubscription.graphql.ts
+++ apps/p99/src/graphql/__generated__/subscribeAndRenderSubscription.graphql.ts
@@ -8,7 +8,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from 'relay-runtime';
+import { ConcreteRequest } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type VisitInput = {
   number: number;
@@ -29,115 +29,109 @@ export type subscribeAndRenderSubscription = {
   variables: subscribeAndRenderSubscription$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = {
-  "defaultValue": null,
-  "kind": "LocalArgument",
-  "name": "name"
-},
-v1 = {
-  "defaultValue": null,
-  "kind": "LocalArgument",
-  "name": "visit"
-},
-v2 = [
-  {
-    "kind": "Variable",
-    "name": "name",
-    "variableName": "name"
-  },
-  {
-    "kind": "Variable",
-    "name": "visit",
-    "variableName": "visit"
-  }
-];
-return {
-  "fragment": {
-    "argumentDefinitions": [
-      (v0/*: any*/),
-      (v1/*: any*/)
-    ],
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "subscribeAndRenderSubscription",
-    "selections": [
+const node: ConcreteRequest = (function () {
+  var v0 = {
+      defaultValue: null,
+      kind: "LocalArgument",
+      name: "name",
+    },
+    v1 = {
+      defaultValue: null,
+      kind: "LocalArgument",
+      name: "visit",
+    },
+    v2 = [
       {
-        "alias": null,
-        "args": (v2/*: any*/),
-        "concreteType": "Workflow",
-        "kind": "LinkedField",
-        "name": "workflow",
-        "plural": false,
-        "selections": [
-          {
-            "args": null,
-            "kind": "FragmentSpread",
-            "name": "renderSubmittedMessageFragment"
-          }
-        ],
-        "storageKey": null
-      }
-    ],
-    "type": "Subscription",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": [
-      (v1/*: any*/),
-      (v0/*: any*/)
-    ],
-    "kind": "Operation",
-    "name": "subscribeAndRenderSubscription",
-    "selections": [
+        kind: "Variable",
+        name: "name",
+        variableName: "name",
+      },
       {
-        "alias": null,
-        "args": (v2/*: any*/),
-        "concreteType": "Workflow",
-        "kind": "LinkedField",
-        "name": "workflow",
-        "plural": false,
-        "selections": [
-          {
-            "alias": null,
-            "args": null,
-            "concreteType": null,
-            "kind": "LinkedField",
-            "name": "status",
-            "plural": false,
-            "selections": [
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "__typename",
-                "storageKey": null
-              }
-            ],
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "id",
-            "storageKey": null
-          }
-        ],
-        "storageKey": null
-      }
-    ]
-  },
-  "params": {
-    "cacheID": "1ca97628bb694c4d5fe00452a2a1d3f8",
-    "id": null,
-    "metadata": {},
-    "name": "subscribeAndRenderSubscription",
-    "operationKind": "subscription",
-    "text": "subscription subscribeAndRenderSubscription(\n  $visit: VisitInput!\n  $name: String!\n) {\n  workflow(visit: $visit, name: $name) {\n    ...renderSubmittedMessageFragment\n    id\n  }\n}\n\nfragment renderSubmittedMessageFragment on Workflow {\n  status {\n    __typename\n  }\n}\n"
-  }
-};
+        kind: "Variable",
+        name: "visit",
+        variableName: "visit",
+      },
+    ];
+  return {
+    fragment: {
+      argumentDefinitions: [v0 /*: any*/, v1 /*: any*/],
+      kind: "Fragment",
+      metadata: null,
+      name: "subscribeAndRenderSubscription",
+      selections: [
+        {
+          alias: null,
+          args: v2 /*: any*/,
+          concreteType: "Workflow",
+          kind: "LinkedField",
+          name: "workflow",
+          plural: false,
+          selections: [
+            {
+              args: null,
+              kind: "FragmentSpread",
+              name: "renderSubmittedMessageFragment",
+            },
+          ],
+          storageKey: null,
+        },
+      ],
+      type: "Subscription",
+      abstractKey: null,
+    },
+    kind: "Request",
+    operation: {
+      argumentDefinitions: [v1 /*: any*/, v0 /*: any*/],
+      kind: "Operation",
+      name: "subscribeAndRenderSubscription",
+      selections: [
+        {
+          alias: null,
+          args: v2 /*: any*/,
+          concreteType: "Workflow",
+          kind: "LinkedField",
+          name: "workflow",
+          plural: false,
+          selections: [
+            {
+              alias: null,
+              args: null,
+              concreteType: null,
+              kind: "LinkedField",
+              name: "status",
+              plural: false,
+              selections: [
+                {
+                  alias: null,
+                  args: null,
+                  kind: "ScalarField",
+                  name: "__typename",
+                  storageKey: null,
+                },
+              ],
+              storageKey: null,
+            },
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "id",
+              storageKey: null,
+            },
+          ],
+          storageKey: null,
+        },
+      ],
+    },
+    params: {
+      cacheID: "1ca97628bb694c4d5fe00452a2a1d3f8",
+      id: null,
+      metadata: {},
+      name: "subscribeAndRenderSubscription",
+      operationKind: "subscription",
+      text: "subscription subscribeAndRenderSubscription(\n  $visit: VisitInput!\n  $name: String!\n) {\n  workflow(visit: $visit, name: $name) {\n    ...renderSubmittedMessageFragment\n    id\n  }\n}\n\nfragment renderSubmittedMessageFragment on Workflow {\n  status {\n    __typename\n  }\n}\n",
+    },
+  };
 })();
 
 (node as any).hash = "c3057cb3b2ce67c2a8d0cf51ff221958";
