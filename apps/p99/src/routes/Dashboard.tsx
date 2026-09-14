e1277ca30e600b048ec684e8069526a1bfe1c91a
Nathanael Leung <84187731+NatLeung96@users.noreply.github.com>
2026-09-14 11:26:34 +0100

    fix(p99): fix authentication issues and workflow queries
    * fix linting
    
    * add login to dashboard
    
    * update values.yaml
    
    * update RelayEnvironment
    
    * update values.yaml
    
    * update supergraph.graphql
    
    * rename workflows context folder to supergraph
    
    * add posix-uid to oauth2 scope
    
    * add tests for Dashboard

--- apps/p99/src/routes/Dashboard.tsx
+++ apps/p99/src/routes/Dashboard.tsx
@@ -1,31 +1,49 @@
 import { Container, Typography, Stack, Button } from "@mui/material";
 import { FileText, ServerCog } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useUserAuth } from "../context/userAuth/useUserAuth";
+import { User } from "@diamondlightsource/sci-react-ui";
 
-const Dashboard = () => (
-  <Container maxWidth={false} sx={{ minHeight: "100vh", mt: 3, mb: 3 }}>
-    <Stack direction="column" alignItems="center" spacing={3}>
-      <Typography variant="h3">Welcome to P99!</Typography>
-      <Button
-        component={Link}
-        to="/Acquisition/Plans"
-        variant="contained"
-        startIcon={<FileText />}
-        sx={{ width: 150, height: 50 }}
-      >
-        Plans
-      </Button>
-      <Button
-        component={Link}
-        to="/Workflows/Workflows"
-        variant="contained"
-        startIcon={<ServerCog />}
-        sx={{ width: 150, height: 50 }}
-      >
-        Workflows
-      </Button>
-    </Stack>
-  </Container>
-);
+const Dashboard = () => {
+  const user = useUserAuth();
+
+  const handleLogIn = () => window.location.assign("/oauth2/sign_in");
+  const handleLogOut = () => window.location.assign("/oauth2/sign_out");
+
+  return (
+    <Container maxWidth={false} sx={{ minHeight: "100vh", mt: 3, mb: 3 }}>
+      <Stack direction="column" alignItems="center" spacing={3}>
+        <Typography variant="h3">Welcome to P99!</Typography>
+        <User
+          onLogin={handleLogIn}
+          onLogout={handleLogOut}
+          user={
+            user.person == null || user.person == undefined
+              ? null
+              : { fedid: user.person }
+          }
+        />
+        <Button
+          component={Link}
+          to="/Acquisition/Plans"
+          variant="contained"
+          startIcon={<FileText />}
+          sx={{ width: 150, height: 50 }}
+        >
+          Plans
+        </Button>
+        <Button
+          component={Link}
+          to="/Workflows/Workflows"
+          variant="contained"
+          startIcon={<ServerCog />}
+          sx={{ width: 150, height: 50 }}
+        >
+          Workflows
+        </Button>
+      </Stack>
+    </Container>
+  );
+};
 
 export default Dashboard;
