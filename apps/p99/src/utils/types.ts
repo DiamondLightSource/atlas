a7226ba6c4094cf15ee43a0cf34581d62c772eea
NatLeung96 <nathanael.leung96@gmail.com>
2026-09-15 10:11:40 +0000

    add types for p99 Tiled data

--- apps/p99/src/utils/types.ts
+++ apps/p99/src/utils/types.ts
@@ -44,3 +44,132 @@ export type WorkflowStatus =
   | "WorkflowSucceededStatus"
   | "WorkflowFailedStatus"
   | "WorkflowErroredStatus";
+
+export type Status = "unknown" | "pending" | "complete" | "running" | "error";
+
+export interface TaskBase {
+  name: string;
+  params: object;
+}
+
+export interface Task extends TaskBase {
+  metadata: object;
+}
+
+export interface TaskListItem {
+  task_id: string;
+  task: Task;
+  request_id: string | null;
+  is_complete: boolean;
+  is_pending: boolean;
+  errors: string[];
+}
+
+export interface TaskListResponse {
+  tasks: TaskListItem[];
+}
+
+export interface TiledResponse {
+  data: TiledData;
+  error: any;
+  links: any;
+  meta: any;
+}
+
+export interface TilePageCount {
+  count: number;
+}
+
+export interface TiledSearchMetadata {
+  start: {
+    plan_name: string;
+    time: number;
+    scan_id: number;
+    scan_file: string;
+  };
+  stop?: unknown;
+}
+
+export interface TiledEntryInfo {
+  id: string;
+  running: boolean;
+}
+
+export interface TiledSearchData {
+  id: string;
+  attributes: { metadata: TiledSearchMetadata };
+}
+
+export interface TiledSearchResponse {
+  data: TiledSearchData[];
+  error: unknown;
+  links: unknown;
+  meta: TilePageCount;
+}
+
+export interface TiledData {
+  id: string;
+  attributes: Attributes;
+  links: Record<string, string>;
+  meta: any;
+}
+
+export interface Attributes {
+  ancestors: string[];
+  structure_family?: any;
+  specs: Spec[];
+  metadata: Metadata;
+  structure: {
+    contents: any;
+    count: number;
+  };
+  access_blob: {
+    tags: string[];
+  };
+  sorting: {
+    key: string;
+    direction: number;
+  }[];
+  data_sources: any;
+}
+
+export interface Spec {
+  name: string;
+  version: string | null;
+}
+
+export interface Metadata {
+  uid: string;
+  time: number;
+  hints: Record<string, { fields: string[] }>;
+  data_keys: Record<string, DataKey>;
+  configuration: Record<string, Configuration>;
+}
+
+export interface DataKey {
+  dtype: any;
+  shape?: number[] | string;
+  units?: string;
+  limits?: Limits;
+  source: string;
+  precision?: number;
+  dtype_numpy: string;
+  object_name?: string;
+}
+
+export interface Limits {
+  control: {
+    low: number;
+    high: number;
+  };
+  display: {
+    low: number;
+    high: number;
+  };
+}
+
+export interface Configuration {
+  data: Record<string, number | string>;
+  data_keys: Record<string, DataKey>;
+  timestamps: Record<string, number>;
+}
