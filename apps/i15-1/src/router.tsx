8114443c8875fe135dbfcfc7803c210fc3373913
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-09-22 18:37:27 +0100

    Address UI niggles

--- apps/i15-1/src/router.tsx
+++ apps/i15-1/src/router.tsx
@@ -35,44 +35,36 @@ const navigation: SectionGroup[] = [
         ],
       },
       {
-        name: "Setup",
-        icon: <SlidersHorizontal />,
-        path: "setup",
-        pages: [
-          {
-            name: "Playlist",
-            element: <Playlist />,
-          },
-          {
-            name: "Samples",
-            element: <div />,
-          },
-          {
-            name: "Pucks",
-            element: <Pucks />,
-          },
-        ],
-      },
-      {
         name: "Acquisition",
         icon: <ScanQrCode />,
         path: "acquisition",
         pages: [
           {
-            name: "Home",
-            element: <div />,
-          },
-          {
             name: "Robot",
             element: <Robot />,
           },
           {
-            name: "Stage",
-            element: <div />,
-          },
-          {
             name: "Plans",
             element: <PlanBrowser />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    sections: [
+      {
+        name: "Setup",
+        icon: <SlidersHorizontal />,
+        path: "setup",
+        pages: [
+          {
+            name: "Pucks",
+            element: <Pucks />,
+          },
+          {
+            name: "Playlist",
+            element: <Playlist />,
           },
         ],
       },
@@ -105,16 +97,6 @@ const navigation: SectionGroup[] = [
             element: <DataVisRedirect />,
           },
         ],
-      },
-    ],
-  },
-  {
-    sections: [
-      {
-        name: "Log",
-        icon: <ClipboardClock />,
-        path: "log",
-        pages: [{ name: "Log", element: <div /> }],
       },
     ],
   },
